1b953951fa38e50a7a28e2e2b2f7acef77f46a93
王玉东 <51770222+wyd19981006@users.noreply.github.com>
2022-07-13 16:46:30 +0800

    实现readonly

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,4 +1,4 @@
-import {reactive} from "../reactive";
+import {reactive,readonly} from "../reactive";
 
 describe('reactive',()=>{
     it('happy path', ()=>{
@@ -7,4 +7,23 @@ describe('reactive',()=>{
         expect(observed).not.toBe(original)
         expect(observed.foo).toBe(1)
     });
+})
+describe('readonly',()=>{
+    it('happy path', () => {
+        // not set
+        const original = { foo: 1, bar: 2 }
+        const wrapped = readonly(original)
+        expect(wrapped).not.toBe(original)
+        expect(wrapped.bar).toBe(2)
+        wrapped.foo = 2
+        // set 后不会更改
+        expect(wrapped.foo).toBe(1)
+    })
+    it('should warn when update readonly prop value', () => {
+        // 这里使用 jest.fn
+        console.warn = jest.fn()
+        const readonlyObj = readonly({ foo: 1 })
+        readonlyObj.foo = 2
+        expect(console.warn).toHaveBeenCalled()
+    })
 })
