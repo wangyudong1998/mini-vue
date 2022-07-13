1a2dfaea97595bce133d3b9e0963c526ebc33b6d
王玉东 <51770222+wyd19981006@users.noreply.github.com>
2022-07-13 09:42:08 +0800

    实现 effect & reactive & 依赖收集 & 触发依赖

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,36 @@
+let activeEffect
+class ReactiveEffect{
+    private _fn: any;
+    constructor(fn){
+        this._fn=fn
+    }
+    run(){
+        activeEffect=this
+        this._fn()
+    }
+}
+export function effect(fn){
+    let _effect=new ReactiveEffect(fn)
+    _effect.run()
+}
+const targetMap=new WeakMap()
+export function track(target,key){
+    let depsMap=targetMap.get(target)
+    if(!depsMap){
+        depsMap=new Map()
+        targetMap.set(target,depsMap)
+    }
+    let dep=depsMap.get(key)
+    if(!dep){
+        dep=new Set()
+        depsMap.set(key,dep)
+    }
+    dep.add(activeEffect)
+}
+export function trigger(target,key){
+    let depsMap=targetMap.get(target)
+    let dep=depsMap.get(key)
+    for (const depElement of dep) {
+        depElement.run()
+    }
+}
