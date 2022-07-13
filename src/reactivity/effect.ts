30751a5cf9f82f2042cd4a820a5cf102f37c70b4
王玉东 <51770222+wyd19981006@users.noreply.github.com>
2022-07-13 14:25:39 +0800

    实现effect的stop&onStop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,43 +1,76 @@
 let activeEffect
-class ReactiveEffect{
-    private _fn: any;
-    public scheduler:Function|undefined
-    constructor(fn,scheduler?){
-        this._fn=fn
-        this.scheduler=scheduler
-    }
-    run(){
-        activeEffect=this
-        return this._fn()
-    }
-}
-export function effect(fn,options:any={}){
-    let _effect=new ReactiveEffect(fn,options.scheduler)
-    _effect.run()
-    return _effect.run.bind(_effect)
 
-}
-const targetMap=new WeakMap()
-export function track(target,key){
-    let depsMap=targetMap.get(target)
-    if(!depsMap){
-        depsMap=new Map()
-        targetMap.set(target,depsMap)
+class ReactiveEffect {
+    private _fn: any;
+    active = true
+    deps = []
+    public scheduler: Function | undefined
+    onStop?: () => void;
+    constructor(fn, scheduler?:Function) {
+        this._fn = fn
+        this.scheduler = scheduler
     }
-    let dep=depsMap.get(key)
-    if(!dep){
-        dep=new Set()
-        depsMap.set(key,dep)
+
+    run() {
+        activeEffect = this
+        const r=this._fn()
+        return r
     }
-    dep.add(activeEffect)
+
+    stop() {
+        cleanupEffect(this)
+    }
 }
-export function trigger(target,key){
-    let depsMap=targetMap.get(target)
-    let dep=depsMap.get(key)
+function cleanupEffect(effect){
+    if(effect.active){
+        effect.deps.forEach((dep: any) => {
+            dep.delete(effect)
+        })
+        if(effect.onStop){
+            effect.onStop()
+        }
+        effect.active = false
+    }
+}
+export function effect(fn, options: any = {}) {
+    const _effect = new ReactiveEffect(fn)
+    Object.assign(_effect,options)
+    _effect.run()
+    const runner: any = _effect.run.bind(_effect)
+    runner.effect = _effect
+    return runner
+}
+
+export function stop(runner) {
+    runner.effect.stop()
+}
+
+const targetMap = new WeakMap()
+
+export function track(target, key) {
+    let depsMap = targetMap.get(target)
+    if (!depsMap) {
+        depsMap = new Map()
+        targetMap.set(target, depsMap)
+    }
+    let dep = depsMap.get(key)
+    if (!dep) {
+        dep = new Set()
+        depsMap.set(key, dep)
+    }
+    if (activeEffect&&activeEffect.active) {
+        dep.add(activeEffect)
+        activeEffect.deps.push(dep)
+    }
+}
+
+export function trigger(target, key) {
+    let depsMap = targetMap.get(target)
+    let dep = depsMap.get(key)
     for (const depElement of dep) {
-        if(depElement.scheduler){
+        if (depElement.scheduler) {
             depElement.scheduler()
-        }else{
+        } else {
             depElement.run()
         }
     }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,5 +1,5 @@
 import {reactive} from "../reactive"
-import {effect} from "../effect"
+import {effect,stop} from "../effect"
 
 describe("effect", () => {
     it("happy path", () => {
@@ -15,43 +15,87 @@ describe("effect", () => {
         expect(nextAge).toBe(22)
     });
 
-    it("effect should return runner", () => {
-        let foo = 10
-        let runner = effect(() => {
+    it('runner', () => {
+        // runner 就是 effect(fn) 返回一个函数，执行该函数就相当于重新执行了一次传入 effect 的 fn
+        // 同时执行 runner 也会将 fn 的返回值返回
+        let foo = 1
+        const runner = effect(() => {
             foo++
             return 'foo'
         })
-        expect(foo).toBe(11)
-        let r = runner()
-        expect(foo).toBe(12)
+        expect(foo).toBe(2)
+        // 调用 runner
+        const r = runner()
+        expect(foo).toBe(3)
+        // 获取 fn 返回的值
         expect(r).toBe('foo')
-    });
+    })
 
-    it("scheduler", () => {
-        let dummy;
-        let run: any;
+    it('scheduler', () => {
+        // 1. scheduler 作为 effect 的一个 option
+        // 2. 有了 scheduler 之后原来的 fn 参数只会执行初始化的一次
+        // 3. 如果依赖更新时不会执行 fn ，而是会去执行 scheduler
+        // 4. runner 不受影响
+        let dummy
+        let run: any
         const scheduler = jest.fn(() => {
-            run = runner;
-        });
-        const obj = reactive({foo: 1});
+            run = runner
+        })
+        const obj = reactive({ foo: 1 })
+        // 在这里将 scheduler 作为一个 option 传入 effect
         const runner = effect(
             () => {
-                dummy = obj.foo;
+                dummy = obj.foo
             },
-            {scheduler}
-        );
-        expect(scheduler).not.toHaveBeenCalled();
-        expect(dummy).toBe(1);
-        // should be called on first trigger
-        obj.foo++;
-        expect(scheduler).toHaveBeenCalledTimes(1);
-        // // should not run yet
-        expect(dummy).toBe(1);
-        // // manually run
-        run();
-        // // should have run
-        expect(dummy).toBe(2);
-    });
+            { scheduler }
+        )
+        expect(scheduler).not.toHaveBeenCalled()
+        // 会执行一次 effect 传入的 fn
+        expect(dummy).toBe(1)
+        obj.foo++
+        // 有了 scheduler 之后，原来的 fn 就不会执行了
+        expect(scheduler).toHaveBeenCalledTimes(1)
+        expect(dummy).toBe(1)
+        run()
+        expect(dummy).toBe(2)
+    })
 
+    it('stop', () => {
+        let dummy
+        const obj = reactive({ prop: 1 })
+        const runner = effect(() => {
+            dummy = obj.prop
+        })
+        obj.prop = 2
+        expect(dummy).toBe(2)
+        // stop 一个 runner 之后
+        stop(runner)
+        obj.prop++
+        // 依赖再次更新，当时传入的 effect 则不会重新执行
+        expect(dummy).toBe(2)
+        // runner 不受到影响
+        runner()
+        expect(dummy).toBe(3)
+    })
+
+    it('onStop', () => {
+        const obj = reactive({
+            foo: 1,
+        })
+        const onStop = jest.fn()
+        let dummy
+        // onStop 是一个函数，也是 effect 的 option
+        const runner = effect(
+            () => {
+                dummy = obj.foo
+            },
+            {
+                onStop,
+            }
+        )
+        // 在调用 stop 的时候，onStop 也会执行
+        stop(runner)
+        expect(onStop).toBeCalledTimes(1)
+    })
 
 })
