3f6fe62ca781e2b43e4095f80dc1784c07cc6199
王玉东 <51770222+wyd19981006@users.noreply.github.com>
2022-07-15 11:26:27 +0800

    优化Stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,40 +1,59 @@
 import {extend} from '../shard'
+
 let activeEffect
+//是否应该收集依赖
+let shouldTrack = false
+
 class ReactiveEffect {
     private _fn: any;
+    // [stop] 该 effect 是否调用过 stop 方法了
+    // true 未调用 false 调用
     active = true
     deps = []
     public scheduler: Function | undefined
     onStop?: () => void;
-    constructor(fn, scheduler?:Function) {
+
+    constructor(fn, scheduler?: Function) {
         this._fn = fn
         this.scheduler = scheduler
     }
 
     run() {
+        if (!this.active) {
+            return this._fn()
+        }
+
+        // 应该收集
+        shouldTrack = true
         activeEffect = this
-        const r=this._fn()
+        const r = this._fn()
+        //reset
+        shouldTrack = false
+
         return r
     }
 
     stop() {
-        cleanupEffect(this)
-    }
-}
-function cleanupEffect(effect){
-    if(effect.active){
-        effect.deps.forEach((dep: any) => {
-            dep.delete(effect)
-        })
-        if(effect.onStop){
-            effect.onStop()
+        if (this.active) {
+            cleanupEffect(this)
         }
-        effect.active = false
     }
 }
+
+function cleanupEffect(effect) {
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect)
+    })
+    if (effect.onStop) {
+        effect.onStop()
+    }
+    effect.active = false
+    effect.deps.length=0
+}
+
 export function effect(fn, options: any = {}) {
     const _effect = new ReactiveEffect(fn)
-    extend(_effect,options)
+    extend(_effect, options)
     _effect.run()
     const runner: any = _effect.run.bind(_effect)
     runner.effect = _effect
@@ -58,10 +77,10 @@ export function track(target, key) {
         dep = new Set()
         depsMap.set(key, dep)
     }
-    if (activeEffect&&activeEffect.active) {
-        dep.add(activeEffect)
-        activeEffect.deps.push(dep)
-    }
+    if (!activeEffect) return
+    if (!shouldTrack) return;
+    dep.add(activeEffect)
+    activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key) {
