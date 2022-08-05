3803dd262f6b0b83cf15290c508e02e1fc8e7172
王玉东 <51770222+wyd19981006@users.noreply.github.com>
2022-08-05 10:06:57 +0800

    更新children(双端对比)

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -6,17 +6,17 @@ import { ref, h } from "../../lib/mini-vue.esm.js";
 // 1. 左侧的对比
 // (a b) c
 // (a b) d e
-const prevChildren = [
-  h("p", { key: "A" }, "A"),
-  h("p", { key: "B" }, "B"),
-  h("p", { key: "C" }, "C"),
-];
-const nextChildren = [
-  h("p", { key: "A" }, "A"),
-  h("p", { key: "B" }, "B"),
-  h("p", { key: "D" }, "D"),
-  h("p", { key: "E" }, "E"),
-];
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+// ];
+// const nextChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "E" }, "E"),
+// ];
 
 // 2. 右侧的对比
 // a (b c)
@@ -33,9 +33,8 @@ const nextChildren = [
 //   h("p", { key: "C" }, "C"),
 // ];
 
-// 3. 新的比老的长
-//     创建新的
-// 左侧
+// 3. 新的比老的长 创建新的
+// 创建右侧
 // (a b)
 // (a b) c
 // i = 2, e1 = 1, e2 = 2
@@ -44,23 +43,22 @@ const nextChildren = [
 //   h("p", { key: "A" }, "A"),
 //   h("p", { key: "B" }, "B"),
 //   h("p", { key: "C" }, "C"),
-//
+//   h("p", { key: "D" }, "D"),
 // ];
 
-// 右侧
+// 创建左侧
 // (a b)
 // c (a b)
 // i = 0, e1 = -1, e2 = 0
-// const prevChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
-// const nextChildren = [
-//   h("p", { key: "C" }, "C"),
-//   h("p", { key: "A" }, "A"),
-//   h("p", { key: "B" }, "B"),
-// ];
+const prevChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
+const nextChildren = [
+  h("p", { key: "C" }, "C"),
+  h("p", { key: "A" }, "A"),
+  h("p", { key: "B" }, "B"),
+];
 
-// 4. 老的比新的长
-//     删除老的
-// 左侧
+// 4. 老的比新的长 删除老的
+// 删除右侧
 // (a b) c
 // (a b)
 // i = 2, e1 = 2, e2 = 1
@@ -71,7 +69,7 @@ const nextChildren = [
 // ];
 // const nextChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
 
-// 右侧
+// 删除左侧
 // a (b c)
 // (b c)
 // i = 0, e1 = 0, e2 = -1
